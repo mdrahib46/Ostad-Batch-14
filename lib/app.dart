937be1addcacc37b0736bde66732841b25b0e651
mamunfarazi142 <mfmamun333@gmail.com>
2025-12-21 16:03:36 +0600

    Practice : Module 11 Class 1

---
+++ lib/app.dart
@@ -0,0 +1,18 @@
+import 'package:batch14/module11-class1/Module11Class1.dart';
+import 'package:flutter/material.dart';
+
+
+
+
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Module11class1(),
+    );
+  }
+}
